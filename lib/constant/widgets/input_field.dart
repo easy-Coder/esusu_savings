d6441c71cb2024c1d907c4d70aea1eb98ecd3d74
easyCoder <b00lit3@outlook.com>
2022-08-29 02:55:10 +0100

    OPTIMISIZE: make input_field extends textformfield

--- lib/constant/widgets/input_field.dart
+++ lib/constant/widgets/input_field.dart
@@ -1,36 +1,21 @@
 import 'package:flutter/material.dart';
 
-class InputFormField extends StatelessWidget {
-  const InputFormField({
-    Key? key,
-    this.onSaved,
-    this.validator,
-    required this.hintText,
-    this.keyboardType,
-    this.obscureText = false,
-  }) : super(key: key);
-
-  final void Function(String? value)? onSaved;
-  final String? Function(String? value)? validator;
-  final String hintText;
-  final TextInputType? keyboardType;
-  final bool obscureText;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: keyboardType,
-      validator: (value) => (validator == null) ? null : validator!(value),
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: Color(0xFFB9B8B8),
-        hintText: hintText,
-        hintStyle: TextStyle(
-          color: Color(0xFF757575),
-        ),
-      ),
-      onSaved: (value) => (onSaved == null) ? null : onSaved!(value),
-      obscureText: obscureText,
-    );
-  }
+class InputFormField extends TextFormField {
+  InputFormField({
+    required hintText,
+    super.key,
+    super.onSaved,
+    super.validator,
+    super.keyboardType,
+    super.obscureText,
+  }) : super(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xFFB9B8B8),
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: Color(0xFF757575),
+            ),
+          ),
+        );
 }
