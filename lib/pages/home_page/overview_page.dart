3aeccf50562681bcb7c1a1f03cef9838a48255ad
easyCoder <b00lit3@outlook.com>
2022-08-29 11:09:30 +0100

    CHANGE: change to slivers

--- lib/pages/home_page/overview_page.dart
+++ lib/pages/home_page/overview_page.dart
@@ -1,3 +1,4 @@
+import 'package:esusu_savings/constant/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 
 import '../../constant/widgets/cards.dart';
@@ -10,226 +11,247 @@ class OverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const Space(
-              height: 80,
+    return CustomScrollView(
+      slivers: [
+        CustomSliverAppBar(
+          title: Text(
+            "Hello, John Doe",
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-            Cards(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24,
-                vertical: 30,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              const Space(
+                height: 80,
               ),
-              color: Theme.of(context).colorScheme.primary,
-              child: Row(
+              Cards(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 30,
+                ),
+                color: Theme.of(context).colorScheme.primary,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Total Deposited",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Space(
+                          height: 10,
+                        ),
+                        Text(
+                          "₦3,000.00",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      height: 30,
+                      thickness: 4,
+                      color: Color(0xffBDBDBD),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Total Withdrawed",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Space(
+                          height: 10,
+                        ),
+                        Text(
+                          "₦3,000.00",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Space(
+                height: 10,
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "Total Deposited",
-                        style: TextStyle(fontSize: 18),
+                  Expanded(
+                    child: Cards(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 12),
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Total Missed",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Space(
+                            height: 10,
+                          ),
+                          Text(
+                            "8",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                      Space(
-                        height: 10,
-                      ),
-                      Text(
-                        "₦3,000.00",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  const Divider(
-                    height: 30,
-                    thickness: 4,
-                    color: Color(0xffBDBDBD),
+                  const Space(
+                    width: 8,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "Total Withdrawed",
-                        style: TextStyle(fontSize: 18),
+                  Expanded(
+                    child: Cards(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 12),
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Personal Savings",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Space(
+                            height: 10,
+                          ),
+                          Text(
+                            "₦9,000.00",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                      Space(
-                        height: 10,
-                      ),
-                      Text(
-                        "₦3,000.00",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ],
               ),
-            ),
-            const Space(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Cards(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 12),
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "Total Missed",
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        Space(
-                          height: 10,
-                        ),
-                        Text(
-                          "8",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const Space(
-                  width: 8,
-                ),
-                Expanded(
-                  child: Cards(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 12),
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "Personal Savings",
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        Space(
-                          height: 10,
-                        ),
-                        Text(
-                          "₦9,000.00",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Space(
-              height: 4,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              const Text(
-                "Personal Goals",
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14,
-                ),
+            ],
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: const Space(
+            height: 4,
+          ),
+        ),
+        SliverToBoxAdapter(
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            const Text(
+              "Personal Goals",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
               ),
-              TextButton(child: const Text("See All"), onPressed: () {}),
-            ]),
-            const Space(
-              height: 4,
             ),
-            SizedBox(
-              child: Column(
-                children: List.generate(
-                  10,
-                  (index) => Cards(
-                    margin: const EdgeInsets.only(bottom: 8.0),
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Row(
+            TextButton(child: const Text("See All"), onPressed: () {}),
+          ]),
+        ),
+        SliverToBoxAdapter(
+          child: const Space(
+            height: 4,
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => Cards(
+              margin: const EdgeInsets.only(bottom: 8.0),
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: const ColoredBox(
+                      color: Colors.blue,
+                      child: SizedBox(
+                        height: 100,
+                        width: 100,
+                      ),
+                    ),
+                  ),
+                  const Space(width: 10),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: const ColoredBox(
-                            color: Colors.blue,
-                            child: SizedBox(
-                              height: 100,
-                              width: 100,
-                            ),
+                        const Text(
+                          "Travel Plan",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
                           ),
                         ),
-                        const Space(width: 10),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                "Travel Plan",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18,
-                                ),
+                        const Space(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "89%",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
                               ),
-                              const Space(
-                                height: 30,
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    "89%",
+                            ),
+                            SizedBox(
+                              child: Row(
+                                children: const [
+                                  Text("You Saved:"),
+                                  Text(
+                                    "₦400",
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 18,
                                     ),
-                                  ),
-                                  SizedBox(
-                                    child: Row(
-                                      children: const [
-                                        Text("You Saved:"),
-                                        Text(
-                                          "₦400",
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
+                                  )
                                 ],
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ),
-                ),
+                ],
               ),
             ),
-            const Space(
-              height: 50,
-            )
-          ],
+            childCount: 10,
+          ),
         ),
-      ),
+        SliverToBoxAdapter(
+          child: const Space(
+            height: 50,
+          ),
+        )
+      ],
     );
   }
 }
