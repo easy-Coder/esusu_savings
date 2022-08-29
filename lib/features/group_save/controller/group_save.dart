a074a4dad7872f53895aa9fe383f190573cd016c
easyCoder <b00lit3@outlook.com>
2022-08-29 22:57:06 +0100

    modified import

--- lib/features/group_save/controller/group_save.dart
+++ lib/features/group_save/controller/group_save.dart
@@ -1,7 +1,7 @@
 import 'package:esusu_savings/constant/error/error.dart';
 import 'package:esusu_savings/features/authenticate/repositries/auth_repositry.dart';
 import 'package:esusu_savings/features/group_save/domain/group/group.dart';
-import 'package:esusu_savings/features/group_save/repositry/savings_repositry.dart';
+import 'package:esusu_savings/features/group_save/repositries/savings_repositry.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
